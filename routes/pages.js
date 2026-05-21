b7eb0f9f33f76b22d3faaee4441185ca366c81eb
Zahraa-Dorgham <zahraaadorgham7704@gmail.com>
2026-05-21 12:49:36 +0100

    v2

--- routes/pages.js
+++ routes/pages.js
@@ -13,24 +13,17 @@ router.get('/', (req, res) => {
 });
 
 router.get('/services', (req, res) => {
-  let data = new Date();
-  let hours = data.getHours();
-  if (hours >= 9 && hours <= 17) {
+ 
     res.render('services');
-  } else {
-    res.render('close');
   }
-});
+);
 
 router.get('/contact', (req, res) => {
-  let data = new Date();
-  let hours = data.getHours();
-  if (hours >= 9 && hours <= 17) {
+ 
     res.render('contact');
-  } else {
-    res.render('close');
   }
-});
+  
+);
 router.get('/menu', (req, res) => {
   res.render('menu');
 });
